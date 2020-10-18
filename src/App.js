9d9ec837441d73c25092626a0fcd68078cb76d11
Carlos Briones <briones.cs@gmail.com>
2020-10-18 17:33:16 -0500

    Changed icon size

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" width="300px" />
+        <img src={logo} className="App-logo" alt="logo" width="150px" />
         <a href="http://www.lowgif.com/fa5f2745ce9ea8c6.html" target="_blank"><img id="editableimage2" src="http://cdn.lowgif.com/full/fa5f2745ce9ea8c6-baby-seal-gifs-find-make-share-gfycat-gifs.gif" border="0" alt="baby seal gifs find make share gfycat gifs"/></a>
         <p>
           Eres arte. <b>PRR</b>
